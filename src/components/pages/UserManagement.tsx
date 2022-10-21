70468dd469d64c4449abc47ed9df5966cc1aeccc
Daisuke Yamamoto <duck921@gmail.com>
2022-10-21 18:16:15 +0800

    create at home

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,11 +1,22 @@
-import {Center, Spinner, Wrap, WrapItem} from "@chakra-ui/react";
-import { memo, FC, useEffect} from "react";
+import {Center, Spinner, useDisclosure, Wrap, WrapItem} from "@chakra-ui/react";
+import { memo, FC, useEffect, useCallback} from "react";
 import {UserCard} from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
+import {UserDetailModal} from "../organisms/user/UserDetaiModal";
+import {useSelectUser} from "../../hooks/useSelectUser";
+import {useLoginUser} from "../../hooks/useLoginUser";
 
 export const UserManagement:FC = memo(() => {
+  const {isOpen, onOpen, onClose} = useDisclosure();
   const {getUsers, loading, users} = useAllUsers();
+  const {onSelectUser, selectedUser} = useSelectUser();
+  const {loginUser} = useLoginUser();
+
   useEffect(()=> getUsers(), [])
+
+  const onClickUser = useCallback((id:number) => {
+    onSelectUser({ id, users, onOpen});
+  }, [users, onSelectUser, onOpen]);
 
   return(
     <>
@@ -15,18 +26,21 @@ export const UserManagement:FC = memo(() => {
         </Center>
       )
       :(
-        <Wrap p={{base:4, md:10}}>
-          {users?.map((user) => (
-            <WrapItem key={user.id}>
+        <Wrap p={{base:4, md:10}} justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id}> 
              <UserCard
+              id={user.id}
               imageUrl="https://source.unsplash.com/random"
               userName={user.username}
-              fullName={user.name} />
+              fullName={user.name}
+              onClick={onClickUser}
+             />
             </WrapItem>
           ))}
         </Wrap>
-      )
-      }
+      )}
+      <UserDetailModal user={selectedUser} isOpen={isOpen} isAdmin={loginUser?.isAdmin} onClose={onClose}/>
     </>
   )
 })
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,11 +2,13 @@ import axios from "axios";
 import {useCallback, useState} from "react"
 import {useHistory} from "react-router-dom";
 import {User} from "../types/api/user";
+import {useLoginUser} from "./useLoginUser";
 import {useMessage} from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
   const {showMessage} = useMessage();
+  const {setLoginUser} = useLoginUser();
 
   //Loading
   const [loading, setLoding] = useState(false);
@@ -17,12 +19,19 @@ export const useAuth = () => {
 
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
       if(res.data){
+      const isAdmin = res.data.id === 10 ? true : false;
+      setLoginUser({...res.data, isAdmin})
       showMessage({title: "logged in", status:"success"})
         history.push("/home")
     } else {
       showMessage({title: "This ID isn't exist", status:"error"})
+      setLoding(false)
     }
-    }).catch(() =>showMessage({title: "Can't login", status:"error"})).finally(() => setLoding(false))
-  }, []);
+    })
+    .catch(() => {
+      showMessage({title: "Can't login", status:"error"})
+      setLoding(false)
+    })
+  }, [history, showMessage, setLoginUser]);
   return { login , loading };
 }
