d3497cfd9b6b5480be4f22e47c2baf5769ed3cc9
motogram <duck921@gmail.com>
2023-01-06 14:08:41 +0800

    Added some text.

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+import { Box, Divider, Flex, Heading, Input, Stack, Text } from "@chakra-ui/react";
 import { memo, FC, useState, ChangeEvent } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
@@ -14,7 +14,7 @@ export const Login: FC = memo(() => {
   const onClickLogin = () => login(userId);
 
   return (
-    <Flex align={"center"} justify={"center"} height={"100vh"}>
+    <Flex align={"center"} justify={"center"} direction={"column"} height={"100vh"}>
       <Box bg={"white"} w={"md"} p={6} borderRadius={"md"} shadow={"md"}>
         <Heading as={"h1"} size={"lg"} textAlign={"center"}>
           User Management App
@@ -34,7 +34,12 @@ export const Login: FC = memo(() => {
             Login
           </PrimaryButton>
         </Stack>
+        <Heading as={"h3"} size={"sm"} textAlign={"center"} color={"gray.300"}>This App is built with React + Typescript + ChakraUI</Heading>
       </Box>
+      <Text fontSize={"md"} mt={4}>Input "10" in user ID field and you can log in as Admin.
+      Edit button show in User Detail modal window.But edit function isn't developed yet.😥</Text>
+      <Text fontSize={"md"} mt={4}>Input "1"~"9" in user ID field and you can log in as guest.
+      </Text>
     </Flex>
   );
 });
